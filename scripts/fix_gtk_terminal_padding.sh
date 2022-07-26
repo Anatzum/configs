117271e066ae95ca101d6a4056bedcf63af5524c
Michael Whitener <mjawhitener@gmail.com>
2022-07-25 23:47:14 -0500

    Moved stuff over from Anatzum/desktop

---
+++ scripts/fix_gtk_terminal_padding.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo -e "vte-terminal {\n  padding: 10px;\n}" > ~/.config/gtk-3.0/gtk.css
---
+++ scripts/zsh_setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mv ~/.zshrc ~/.zshrc.bak
+cp ../files/.zshrc ~/.zshrc
